43f56036b84fcc3bb2d30fdadc709d6052ebea8c
Antoine Contoux <contoux1u@etu.univ-lorraine.fr>
2022-01-31 11:20:43 +0100

    init

---
+++ src/EvenementArriveePassagerPalier.java
@@ -0,0 +1,50 @@
+public class EvenementArriveePassagerPalier extends Evenement {
+    /* APP: Arrivée Passager Palier
+       L'instant précis ou un nouveau passager arrive sur un palier donné, dans le but
+       de monter dans la cabine.
+    */
+    
+    private Etage etage;
+
+    public EvenementArriveePassagerPalier(long d, Etage edd) {
+    	super(d);
+		etage = edd;
+    }
+
+    public void afficheDetails(StringBuilder buffer, Immeuble immeuble) {
+		buffer.append("APP ");
+		buffer.append(etage.numero());
+    }
+
+    public void traiter(Immeuble immeuble, Echeancier echeancier) {
+		assert etage != null;
+		assert immeuble.etage(etage.numero()) == etage;
+		Passager p = new Passager(date, etage, immeuble);
+		Cabine c = immeuble.cabine;
+	
+		notYetImplemented();
+		/* 
+		if (c.porteOuverte && c.étage == étage) {
+		    if (c.intention() == '-') {
+			//notYetImplemented();
+			c.changerIntention(p.sens());
+			echeancier.ajouter(new EvenementFermeturePorteCabine(date + tempsPourOuvrirOuFermerLesPortes)); 
+			char fmp = c.faireMonterPassager(p);
+			// Faudrait aussi ajouter le premier PCP...
+			if (fmp == 'O') {
+			    assert true;
+			} else {
+			    assert false : "else impossible";
+			};	
+		    } else {
+			notYetImplemented();
+		    };
+		} else {
+		    notYetImplemented();
+		};
+		//*/
+	
+		assert c.intention() != '-' : "intention impossible";
+    }
+
+}
---
+++ src/EvenementPietonArrivePalier.java
@@ -0,0 +1,27 @@
+public class EvenementPietonArrivePalier extends Evenement {
+    /* PAP: Pieton Arrive Palier
+       L'instant précis ou un passager qui à décidé de continuer à pieds arrive sur un palier donné.
+    */
+
+    private Etage etage;
+    private Passager passager;
+
+    public void afficheDetails(StringBuilder buffer, Immeuble immeuble) {
+		buffer.append("PAP ");
+		buffer.append(etage.numero());
+		buffer.append(" #");
+		buffer.append(passager.numeroDeCreation);
+    }
+    
+    
+    public void traiter(Immeuble immeuble, Echeancier echeancier) {
+    	notYetImplemented();
+    }
+
+    public EvenementPietonArrivePalier(long d, Etage edd, Passager pa) {
+	super(d);
+		etage = edd;
+		passager = pa;
+    }
+    
+}
