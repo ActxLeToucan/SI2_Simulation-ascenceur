7d346ea903157e4fe06e7071c5aa384f413236d1
Antoine Contoux <contoux1u@etu.univ-lorraine.fr>
2022-02-02 10:22:11 +0100

    oui

--- src/EvenementArriveePassagerPalier.java
+++ src/EvenementArriveePassagerPalier.java
@@ -1,5 +1,5 @@
 public class EvenementArriveePassagerPalier extends Evenement {
-    /* APP: Arrivée Passager Palier
+    /* APP : Arrivée Passager Palier
        L'instant précis ou un nouveau passager arrive sur un palier donné, dans le but
        de monter dans la cabine.
     */
@@ -21,28 +21,26 @@ public class EvenementArriveePassagerPalier extends Evenement {
 		assert immeuble.etage(etage.numero()) == etage;
 		Passager p = new Passager(date, etage, immeuble);
 		Cabine c = immeuble.cabine;
-	
-		notYetImplemented();
-		/* 
-		if (c.porteOuverte && c.étage == étage) {
+
+		//notYetImplemented();
+		if (c.porteOuverte && c.etage == etage) {
 		    if (c.intention() == '-') {
-			//notYetImplemented();
-			c.changerIntention(p.sens());
-			echeancier.ajouter(new EvenementFermeturePorteCabine(date + tempsPourOuvrirOuFermerLesPortes)); 
-			char fmp = c.faireMonterPassager(p);
-			// Faudrait aussi ajouter le premier PCP...
-			if (fmp == 'O') {
-			    assert true;
-			} else {
-			    assert false : "else impossible";
-			};	
+				//notYetImplemented();
+				c.changerIntention(p.sens());
+				echeancier.ajouter(new EvenementFermeturePorteCabine(date + tempsPourOuvrirOuFermerLesPortes));
+				char fmp = c.faireMonterPassager(p);
+				// Il faudrait aussi ajouter le premier PCP...
+				if (fmp == 'O') {
+					assert true;
+				} else {
+					assert false : "else impossible";
+				}
 		    } else {
-			notYetImplemented();
-		    };
+				notYetImplemented();
+		    }
 		} else {
 		    notYetImplemented();
-		};
-		//*/
+		}
 	
 		assert c.intention() != '-' : "intention impossible";
     }
--- src/EvenementFermeturePorteCabine.java
+++ src/EvenementFermeturePorteCabine.java
@@ -1,9 +1,9 @@
 public class EvenementFermeturePorteCabine extends Evenement {
-    /* FPC: Fermeture Porte Cabine
+    /* FPC : Fermeture Porte Cabine
        L'instant précis ou la porte termine de se fermer.
        Tant que la porte n'est pas complètement fermée, il est possible pour un passager
        de provoquer la réouverture de la porte. Dans ce cas le FPC est décalé dans le temps
-       selon la méthode decalerFPC qui est dans l'échéancier.
+       selon la méthode decaler FPC qui est dans l'échéancier.
     */
 
     public EvenementFermeturePorteCabine(long d) {
@@ -18,9 +18,9 @@ public class EvenementFermeturePorteCabine extends Evenement {
 		Cabine cabine = immeuble.cabine;
 		assert cabine.porteOuverte : "precondition";
 	
-		//cabine.porteOuverte = false;
-		notYetImplemented();
-	
+		cabine.porteOuverte = false;
+		//notYetImplemented();
+
 		assert (! cabine.porteOuverte) : "postcondition";
     }
 
